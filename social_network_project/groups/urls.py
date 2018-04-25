17d098c54a629a470ffedaf9328a0267d154c611
enzo9 <enzo.martino.94@gmail.com>
2018-04-25 16:48:41 +0200

    bug fixing

--- social_network_project/groups/urls.py
+++ social_network_project/groups/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 app_name = 'groups'
 
 urlpatterns = [
-   path('', views.ListGroups.as_view(), 'all'),
+   path('', views.ListGroups.as_view(), name = 'all'),
    path('new/', views.CreateGroup.as_view(), name = 'create'),
    path('posts/in/<slug:slug>/', views.SingleGroup.as_view(), name = 'single'),
    path('join/<slug:slug>/', views.JoinGroup.as_view(), name = 'join'),
