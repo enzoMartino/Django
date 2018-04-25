615be849355353309f24de099a3f55fa73598319
enzo9 <enzo.martino.94@gmail.com>
2018-04-25 16:32:24 +0200

    fixed static link

--- social_network_project/social_network_project/settings.py
+++ social_network_project/social_network_project/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+print(STATIC_DIR)
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
@@ -123,8 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [
+    STATIC_DIR,
 ]
 
 LOGIN_REDIRECT_URL = 'test'
