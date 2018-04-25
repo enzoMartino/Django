3e012384dfa59922dad9d2cc4d581664d3f0c3ed
enzo9 <enzo.martino.94@gmail.com>
2018-04-25 14:39:20 +0200

    created groups' views and filled groups' templates

--- social_network_project/groups/views.py
+++ social_network_project/groups/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
+from . import models
 
 # Create your views here.
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+
+    fields = ('name', 'description')
+
+    model = models.Group
+
+
+class SingleGroup(generic.DeleteView):
+
+    model = models.Group
+
+
+class ListGroups(generic.ListView):
+
+    model = models.Group
