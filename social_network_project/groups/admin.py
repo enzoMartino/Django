c47edc9d3c090bd03685a2080af54fa745e64e42
enzo9 <enzo.martino.94@gmail.com>
2018-04-26 17:15:44 +0200

    added custom admin model fields order

--- social_network_project/groups/admin.py
+++ social_network_project/groups/admin.py
@@ -3,8 +3,15 @@ from . import models
 
 # Register your models here.
 
+class GroupAdmin(admin.ModelAdmin):
+
+    fields = ['description', 'name', 'slug',]
+
+
 class GroupMemberInline(admin.TabularInline):
 
     model = models.GroupMember
 
-admin.site.register(models.Group)
+admin.site.register(models.Group, GroupAdmin)
+
+admin.site.register(models.GroupMember)
